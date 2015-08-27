41ea5159eb4c968c191bc1a7f6ca84be85d66316
Rene Hangstrup Møller <rhmoller@gmail.com>
2015-08-28 01:05:48 +0200

    reversed physical bullets

--- src/main.js
+++ src/main.js
@@ -45,6 +45,10 @@ document.body.appendChild(canvas);
 let bouncy = { x: 100, y: 100, w: 16, h: 16, dx: 2, dy: 3 };
 let block = { x: 200, y: 200, w: 16, h: 16 };
 
+let bullets = new Set();
+
+let bulletTime = 0;
+
 let keys = {
   up: false,
   down: false,
@@ -73,21 +77,59 @@ window.addEventListener("keyup", (e) => {
   }
 }, false);
 
-function update() {
+let lastTime = 0;
+
+function update(timestamp) {
     requestAnimationFrame(update);
+    let dt = timestamp - lastTime;
 
     if (keys.left) {
-      block.x -= 2;
+      block.x -= 4;
     }
     if (keys.up) {
-      block.y -= 2;
+      block.y -= 4;
     }
     if (keys.right) {
-      block.x += 2;
+      block.x += 4;
     }
     if (keys.down) {
-      block.y += 2;
+      block.y += 4;
     }
+
+    if (keys.fire && (timestamp - bulletTime) > 100) {
+      bullets.add({ x : WIDTH + 100 - 50 * Math.random(), y : block.y + 100 - 50 * Math.random(), vx: -6, vy: 0 });
+      bullets.add({ x : WIDTH + 100 - 50 * Math.random(), y : block.y + 100 - 50 * Math.random(), vx: -6, vy: 0 });
+      bullets.add({ x : WIDTH + 100 - 50 * Math.random(), y : block.y + 100 - 50 * Math.random(), vx: -6, vy: 0 });
+      bulletTime = timestamp;
+    }
+
+    for (let bullet of bullets) {
+      let b2px = block.x - bullet.x;
+      let b2py = block.y - bullet.y;
+      let d = Math.sqrt(b2px * b2px + b2py * b2py);
+      let a = Math.atan2(b2py, b2px);
+      let ax = Math.cos(a);
+      let ay = Math.sin(a);
+
+      bullet.vx += ax;
+      bullet.vy += ay;
+
+      let v = Math.sqrt(bullet.vx * bullet.vx + bullet.vy * bullet.vy);
+      if (v > 6) {
+        let a = Math.atan2(bullet.vy, bullet.vx);
+        bullet.vx = 6 * Math.cos(a);
+        bullet.vy = 6 * Math.sin(a);
+      }
+
+      bullet.x += bullet.vx;
+      bullet.y += bullet.vy;
+
+      if (bullet.x < 0 || bullet.y > HEIGHT + 100 || bullet.y < -100 || d < 12) {
+        bullets.delete(bullet);
+      }
+    }
+
+    document.title = `Bullets: ${bullets.size}`
 
     bouncy.x += bouncy.dx;
     bouncy.y += bouncy.dy;
@@ -106,10 +148,18 @@ function update() {
     ctx.fillStyle = "#000";
     ctx.fillRect(block.x, block.y, block.w, block.h);
 
+    ctx.fillStyle = "#ff0";
+    for (let bullet of bullets) {
+      ctx.fillRect(bullet.x, bullet.y, 8, 8);
+    }
+
     ctx.drawImage(preloader.get("giddy.png"), 0, 0);
     ctx.drawImage(preloader.get("giddy2.png"), 20, 0);
+
+    lastTime = timestamp;
 }
 
 preloader.load(()=> {
+  lastTime = Performance.now;
   requestAnimationFrame(update);
 });
